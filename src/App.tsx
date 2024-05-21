da60e7d704eb2c294c9545ca0e039976b7ec7240
acastelli <alecastellin@gmail.com>
2024-05-21 00:30:03 -0300

    fix basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import theme from "./muiTheme";
 import { ResolutionProvider } from "./features/common/hooks/useResolution";
 import MainRouter from "./features/common/components/MainRouter";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
+      <Router basename="/tudupla">
         <ResolutionProvider>
           <MainRouter />
         </ResolutionProvider>
